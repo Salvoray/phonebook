09b496169e56f0108d8a07c1aa3a753679c8d5de
Salvoray <zzoomikk@gmail.com>
2021-05-28 22:10:40 +0300

    Update App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
 import { Component } from "react";
+import PhoneBookForm from "./components/ContactForm";
+import ContactFilter from "./components/ContactFilter";
+import ContactList from "./components/ContactList";
 
 class App extends Component {
   state = {
-    contacts: [],
-    name: "",
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  };
+
+  addContact = (contact) => {
+    this.state.contacts.map((contact) => contact.name).includes(contact.name)
+      ? alert(`${contact.name} is already in contacts`)
+      : this.setState(({ contacts }) => ({
+          contacts: [contact, ...contacts],
+        }));
+  };
+
+  changeFilter = (e) => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getFilteredContacts = () => {
+    const lowerCaseFilter = this.state.filter.toLowerCase();
+    return this.state.contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(lowerCaseFilter)
+    );
+  };
+
+  deleteContact = (contactId) => {
+    this.setState(({ contacts }) => ({
+      contacts: contacts.filter((contact) => contact.id !== contactId),
+    }));
   };
 
   render() {
     return (
       <div>
-        <h2>Main Page Phone Book</h2>
+        <h1>PhoneBook</h1>
+        <PhoneBookForm onSubmit={this.addContact} />
+        <h1>Contacts</h1>
+        <ContactFilter value={this.state.filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={this.getFilteredContacts()}
+          onDeleteContact={this.deleteContact}
+        />
       </div>
     );
   }
